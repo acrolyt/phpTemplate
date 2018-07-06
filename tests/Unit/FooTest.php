bad89f072f5693704a68246df59359f354fd5452
Marco Wegner <marco.wegner@postcon.de>
2018-07-06 13:30:07 +0200

    [initial] before i forget

---
+++ tests/Unit/FooTest.php
@@ -0,0 +1,21 @@
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marco
+ * Date: 06.07.18
+ * Time: 13:19
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class FooTest extends TestCase
+{
+
+    /**
+     * @test
+     */
+    function it_tests_something() {
+        self::assertNotNull(1);
+    }
+}
